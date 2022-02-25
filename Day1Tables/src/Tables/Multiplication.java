577f4544bdbb25d0eb767ae1438a699088f46fc8
Fam0148 <subashssubramaniank@gmail.com>
2022-02-25 11:07:12 +0530

    Multiplication Tables

---
+++ Day1Tables/src/Tables/Multiplication.java
@@ -0,0 +1,17 @@
+package Tables;
+import java.util.*;
+public class Multiplication 
+{
+	public static void main(String args[])
+	{
+	int EndNumber = 10;
+	System.out.print("Enter the Number ");
+	Scanner Input = new Scanner(System.in);
+	int Number = Input.nextInt();
+	for(int i=1;i<=EndNumber;i++)
+	{
+		System.out.println(i + "*" + EndNumber + "=" +(EndNumber*i));
+	}
+	}
+	
+}
